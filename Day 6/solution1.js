4647f6b72267031e7a814da9dee206bcc143df63
Ibrahim-Yousif <ibrahimyousif12348@gmail.com>
2022-12-11 10:09:35 +0300

    solved upto day 6

---
+++ Day 6/solution1.js
@@ -0,0 +1,17 @@
+const input = require('./input.js');
+
+let temp = [];
+input.map((elm, idx) => {
+    if(temp.length == 4){
+        isDifferent(temp, idx)
+    }
+    temp.push(elm);
+})
+
+function isDifferent(arr, idx){
+    if((new Set(arr)).size !== arr.length){ // instead of brute forcing arr[0] == arr[1] || arr[0] == arr[2] || arr[0] == arr[3] || arr[1] == arr[2] || arr[1] == arr[3] || arr[2] == arr[3]
+        arr.shift();
+        return;
+    }
+    console.log(idx)
+}
